80c419174d16fa5a9b67e0774caefc805b35ba60
Arkhun Abdullazade <arkhun2008@gmail.com>
2024-01-25 21:24:07 +0400

    Delete Everything

--- Sql/Tables.sql
+++
@@ -1,9 +0,0 @@
-use MyMovieCollection;
-
-create table Movies(
-    [Id] int primary key identity,
-    [Title] nvarchar(100),
-    [Description] text,
-    [ImbdScore] float,
-    [MetaScore] int,
-)
